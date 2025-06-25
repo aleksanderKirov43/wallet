0ee446b60f9c2a11a0a368dd819e61768fa67619
aleksanderKirov43 <region-manager43@yandex.ru>
2025-06-25 23:45:06 +0300

    up method service\repo

--- internal/repositiory/repository.go
+++ internal/repositiory/repository.go
@@ -3,6 +3,7 @@ package repositiory
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"wallet/internal/service"
 )
 
@@ -25,16 +26,59 @@ func NewRepositoryDB(db *sql.DB) *RepositoryDB {
 
 func (r *RepositoryDB) CreateWallet(ctx context.Context, wallet *service.Wallet) error {
 
+	query := `INSERT INTO wallet_db (walletId, operationType, amount) VALUES ($1, $2, $3)`
+
+	_, err := r.db.ExecContext(ctx, query, wallet.WalletID, wallet.OperationType, wallet.Amount)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *RepositoryDB) GetBalance(ctx context.Context, walletID int) (*service.Wallet, error) {
 
+	query := `SELECT operationType, amount FROM wallet_db WHERE walletId = $1`
+
+	row := r.db.QueryRowContext(ctx, query, walletID)
+
+	var operationType string
+	var amount int64
+
+	err := row.Scan(&operationType, &amount)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("Кошелек с ID %d не найден", walletID)
+		}
+		return nil, err
+	}
+
+	return &service.Wallet{
+		WalletID:      walletID,
+		OperationType: operationType,
+		Amount:        amount,
+	}, nil
 }
 
 func (r *RepositoryDB) UpdateBalance(ctx context.Context, wallet *service.Wallet) error {
 
+	query := "UPDATE wallet_db SET operationType = $1, amount = $2 WHERE walletId = $3"
+
+	_, err := r.db.ExecContext(ctx, query, wallet.OperationType, wallet.Amount, wallet.WalletID)
+	if err != nil {
+		return fmt.Errorf("Не удалось обновить баланс: %w", err)
+	}
+
+	return nil
+
 }
 
 func (r *RepositoryDB) DeleteWallet(ctx context.Context, walletID int) error {
+	query := "DELETE FROM wallet_db WHERE walletId = $1"
 
+	_, err := r.db.ExecContext(ctx, query, walletID)
+	if err != nil {
+		return fmt.Errorf("не удалось удалить кошелёк: %w", err)
+	}
+
+	return nil
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -2,14 +2,16 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"wallet/internal/repositiory"
 )
 
 type WalletService interface {
 	CreateWallet(ctx context.Context, req *Wallet) (*Wallet, error)
-	PostBalance(ctx context.Context, walletID int) (*Wallet, error)
+	PostBalance(ctx context.Context, req *Wallet) (*Wallet, error)
 	GetBalance(ctx context.Context, walletID int) (*CheckBalance, error)
-	UpdateBalance(ctx context.Context, walletID int) (*Wallet, error)
+	UpdateBalance(ctx context.Context, req *Wallet) (*Wallet, error)
 	DeleteWallet(ctx context.Context, walletID int) error
 }
 
@@ -20,7 +22,7 @@ type CheckBalance struct {
 type Wallet struct {
 	WalletID      int    `json:"walletId"`
 	OperationType string `json:"operationType"`
-	Amount        int64  `json:"amount"`
+	Amount        int64  `json:"amount,omitempty"`
 }
 
 type WalletServiceImpl struct {
@@ -35,20 +37,84 @@ func NewWalletService(repo repositiory.Repository) *WalletServiceImpl {
 
 func (s *WalletServiceImpl) CreateWallet(ctx context.Context, req *Wallet) (*Wallet, error) {
 
+	if req.OperationType != "DEPOSIT" {
+		return nil, errors.New("Сначала внесите депозит")
+	}
+
+	err := s.repo.CreateWallet(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
 }
 
-func (s *WalletServiceImpl) PostBalance(ctx context.Context, walletID int) (*Wallet, error) {
+func (s *WalletServiceImpl) PostBalance(ctx context.Context, req *Wallet) (*Wallet, error) {
 
+	exist, err := s.repo.GetBalance(ctx, req.WalletID)
+	if err != nil {
+		return nil, fmt.Errorf("Кошелёк не найден: %w", err)
+	}
+
+	switch req.OperationType {
+	case "DEPOSIT":
+		exist.Amount += req.Amount
+	case "WITHDRAW":
+		return nil, errors.New("Метод не предназначен для вывода средств, выбирите : UpdateBalance")
+	default:
+		return nil, errors.New("Неизвестный тип операции")
+	}
+
+	err = s.repo.UpdateBalance(ctx, exist)
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка пополнения депозита: %w", err)
+	}
+
+	return exist, nil
 }
 
 func (s *WalletServiceImpl) GetBalance(ctx context.Context, walletID int) (*CheckBalance, error) {
 
+	exist, err := s.repo.GetBalance(ctx, walletID)
+	if err != nil {
+		return nil, fmt.Errorf("Кошелёк не найден: %w", err)
+	}
+
+	return &CheckBalance{Amount: exist.Amount}, nil
 }
 
-func (s *WalletServiceImpl) UpdateBalance(ctx context.Context, walletID int) (*Wallet, error) {
+func (s *WalletServiceImpl) UpdateBalance(ctx context.Context, req *Wallet) (*Wallet, error) {
+
+	exist, err := s.repo.GetBalance(ctx, req.WalletID)
+	if err != nil {
+		return nil, fmt.Errorf("Кошелёк не найден: %w", err)
+	}
+
+	switch req.OperationType {
+	case "DEPOSIT":
+		exist.Amount += req.Amount
+	case "WITHDRAW":
+		if exist.Amount < req.Amount {
+			return nil, errors.New("Недостаточно средств для вывода")
+		}
+		exist.Amount -= req.Amount
+	default:
+		return nil, errors.New("Неизвестный тип операции")
+	}
+
+	err = s.repo.UpdateBalance(ctx, exist)
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка обновления данных: %w", err)
+	}
+
+	return exist, nil
 
 }
 
 func (s *WalletServiceImpl) DeleteWallet(ctx context.Context, walletID int) error {
-
+	err := s.repo.DeleteWallet(ctx, walletID)
+	if err != nil {
+		return fmt.Errorf("ошибка удаления кошелька: %w", err)
+	}
+	return nil
 }
